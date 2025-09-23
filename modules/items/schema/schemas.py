1a5d7226a4b8aa484e95441016e40a5b745659bc
zyronic275 <zyronic275@gmail.com>
2025-09-23 11:12:22 +0700

    Week 4

--- modules/items/schema/schemas.py
+++ modules/items/schema/schemas.py
@@ -1,8 +1,20 @@
 from typing import Optional
 from pydantic import BaseModel
 
+# Windsurf: Refactor | Explain
 class Item(BaseModel):
     id: int
     name: str
     description: Optional[str] = None
     price: float
+
+# Windsurf: Refactor | Explain
+class ItemResponse(BaseModel):
+    id: int
+    name: str
+
+# Windsurf: Refactor | Explain
+class ResponseModel(BaseModel):
+    success: bool
+    message: str
+    data: ItemResponse
